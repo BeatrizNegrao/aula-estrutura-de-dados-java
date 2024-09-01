77abdc41cb2648b2a660330311f64566e270dfb0
BeatrizNegrao <beatrizfranciscanegrao@gmail.com>
2024-08-31 23:24:35 -0300

    update code

---
+++ EstruturaDados1.java
@@ -0,0 +1,22 @@
+// 1.1) Crie um método que receba 1 vetor com 8 posições, inverta a ordem dos itens e retorne o vetor invertido.
+public class EstruturaDados1 {
+    public static void main(String[] args) {
+        int vetor[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        for (int i = 0; i < 8; i++) {
+            System.out.println(vetor[i]);
+        }
+        System.out.println("========================================");
+        inverteVetor(vetor);
+    }
+
+    public static void inverteVetor(int v[]) {
+
+        int aux[] = new int[8];
+
+        for (int i = 7; i >= 0; i--) {
+            aux[7 - i] = v[i];
+            System.out.println(aux[7 - i]);
+
+        }
+    }
+}
