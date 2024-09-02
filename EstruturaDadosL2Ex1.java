6c98855de556a6a0537c93f110f52633f4e56160
BeatrizNegrao <beatrizfranciscanegrao@gmail.com>
2024-09-02 17:22:18 -0300

    Primeiro exercício da lista 2

--- EstruturaDadosL2Ex1.java
+++ EstruturaDadosL2Ex1.java
@@ -1,3 +1,18 @@
+// Crie um método recursivo que receba um valor inteiro decimal e retorne uma String com o valor correspondente em binário.
 public class EstruturaDadosL2Ex1 {
-    
+    public static void main(String[] args) {
+        System.out.println("Valor: ");
+        decimalToBinario(20);
+        System.out.println("");
+        }
+
+        public static void decimalToBinario(int valor) {
+           if(valor <= 2){
+            System.out.println(valor/2);
+            System.out.println(valor%2);
+           } else{
+            decimalToBinario(valor/2);
+            System.out.println(valor%2);
+           }
+        }
 }
